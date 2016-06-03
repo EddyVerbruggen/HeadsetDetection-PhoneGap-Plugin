8d264fd558ca7f418ed811a468de2f49842d0bc6
Gerhard Sletten <gerhardsletten@gmail.com>
2016-06-03 14:20:44 +0200

    Wrong plugin wrapper

--- www/HeadsetDetection.js
+++ www/HeadsetDetection.js
@@ -1,18 +1,18 @@
-cordova.define("cordova-plugin-headsetdetection.HeadsetDetection", function(require, exports, module) {
-  var HeadsetDetection = {
-    detect: function (successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "HeadsetDetection", "detect", []);
-    },
-    registerRemoteEvents: function(actionCallback) {
-      this.actionCallback = actionCallback;
-    },
-    remoteHeadsetRemoved: function() {
-      this.actionCallback && this.actionCallback('headsetRemove');
-    },
-    remoteHeadsetAdded: function() {
-      this.actionCallback && this.actionCallback('headsetAdded');
-    }
-  };
+var exec = require('cordova/exec');
 
-  module.exports = HeadsetDetection;
-});
+var HeadsetDetection = {
+  detect: function (successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "HeadsetDetection", "detect", []);
+  },
+  registerRemoteEvents: function(actionCallback) {
+    this.actionCallback = actionCallback;
+  },
+  remoteHeadsetRemoved: function() {
+    this.actionCallback && this.actionCallback('headsetRemove');
+  },
+  remoteHeadsetAdded: function() {
+    this.actionCallback && this.actionCallback('headsetAdded');
+  }
+};
+
+module.exports = HeadsetDetection;
