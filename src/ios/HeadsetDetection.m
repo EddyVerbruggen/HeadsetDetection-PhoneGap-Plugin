b6e800679c3d4554b6863010e8235ca0a2be76ec
Julian Laval <hello@julianlaval.com>
2019-06-11 17:46:00 +0100

    Restricted headset check to headphone jack only

--- src/ios/HeadsetDetection.m
+++ src/ios/HeadsetDetection.m
@@ -28,10 +28,10 @@
 - (BOOL) isHeadsetEnabled {
   AVAudioSessionRouteDescription* route = [[AVAudioSession sharedInstance] currentRoute];
   for (AVAudioSessionPortDescription* desc in [route outputs]) {
-    if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones] ||
-        [[desc portType] isEqualToString:AVAudioSessionPortBluetoothHFP] ||
-        [[desc portType] isEqualToString:AVAudioSessionPortBluetoothA2DP] ||
-        [[desc portType] isEqualToString:AVAudioSessionPortBluetoothLE]) {
+    if ([[desc portType] isEqualToString:AVAudioSessionPortHeadphones]) {
+      // [[desc portType] isEqualToString:AVAudioSessionPortBluetoothHFP] ||
+      // [[desc portType] isEqualToString:AVAudioSessionPortBluetoothA2DP] ||
+      // [[desc portType] isEqualToString:AVAudioSessionPortBluetoothLE]
       return YES;
     }
   }
